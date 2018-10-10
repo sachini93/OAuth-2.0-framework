75c2bcf02e1901f805ac01ce4cef7b8f3d840396
sachini93 <sachini.thaksala@gmail.com>
2018-10-11 05:18:45 +0530

    initial commit
    added OAuth app

---
+++ src/main/java/example/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package example.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "example")
+public class HelloWorldConfiguration {
+	
+
+}
